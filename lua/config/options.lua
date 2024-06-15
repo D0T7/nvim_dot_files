4b8ff44f8867dc757371efd193b15802d7d06b11
Asim Ali Khan <uze761@gmail.com>
2024-06-16 00:04:24 +0530

    linux nvim config
    linux nvim config with golang, python working properly
    theme working
    lualine custom function working

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local map = vim.keymap.set
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+vim.opt.shell = "/usr/bin/zsh"
